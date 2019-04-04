9df5899536c38768e7f4e5f3678c8086246dd924
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2019-04-03 23:13:15 -0400

    Added (hopefully working) search functionality.
    Unable to test due to server hosting the database refusing connections.

--- customer/search.php
+++ customer/search.php
@@ -23,6 +23,7 @@
         Minimum Star Rating: <input type="number" name="minimumStars" value="0"/><br/>
         Minimum Capacity: <input type="number" name="minimumCapacity" value="0"/><br/>
         Hotel Chain: <select name="hotelChain">
+            <option value="">Any</option>
         <?php
             include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
             
@@ -39,13 +40,14 @@
 
         </select><br/>
         Minimum number of Rooms: <input type="number" name="minimumRooms" value="0"/><br/>
-        Maximum Price: <input type="number" name="maximumPrice"/><br/>
+        Maximum Price: <input type="number" name="maximumPrice" value="0"/><br/>
         <input type="submit" value="Search"/>
 </form>
 
 <br/>
 
 We need to search for:<br/><br/>
+<ul>
 <?php
 echo "City: " . $_POST['city'] . "<br/>";
 echo "Minimum Starts: " . $_POST['minimumStars'] . "<br/>";
@@ -53,7 +55,54 @@ echo "minimum Capacity: " . $_POST['minimumCapacity'] . "<br/>";
 echo "Hotel Chain: " . $_POST['hotelChain'] . "<br/>";
 echo "Minimum number of Rooms: " . $_POST['minimumRooms'] . "<br/>";
 echo "Maximum Price: " . $_POST['maximumPrice'] . "<br/>";
+include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
+
+$city = $_POST['city'];
+$minimumStars = $_POST['minimumStars'];
+$minimumCapacity = $_POST['minimumCapacity'];
+$hotelChain = $_POST['hotelChain'];
+$minimumRooms = $_POST['minimumRooms'];
+$maximumPrice = $_POST['maximumPrice'];
+
+$room_query = "SET search_path = 'HotelSystem'; SELECT * FROM room WHERE city=$city";
+
+if($minimumStars > 0) {
+    $room_query = $room_query . " AND (SELECT hotel_id, stars FROM hotel WHERE (hotel.hotel_id = room.hotel_id AND hotel.stars >= $minimumStars))";
+}
+
+if($minimumCapacity > 0) {
+    $room_query = $room_query . " AND capacity >= $minimumCapacity";
+}
+
+if($hotelChain != "Any") {
+    $room_query = $room_query . " AND (SELECT hotel_id, hotel_chain_id FROM hotel_chain WHERE (hotel_chain.hotel_id = room.hotel_id AND hotel_chain.hotel_chain_id = $hotelChain))";
+}
+
+if($minimumRooms > 0) {
+    $room_query = $room_query . " AND (SELECT hotel_id, number_of_rooms FROM hotel WHERE (hotel.hotel_id = room.hotel_id AND hotel.number_of_rooms = $minimumRooms))";
+}
+
+if($maximumPrice > 0) {
+    $room_query = $room_query . " AND price <= $maximumPrice";
+}
+
+$result = pg_query($room_query . ";");
+
+while ($room = pg_fetch_row($result, null, PGSQL_ASSOC)) {
+
+    $room_number = $room['room_number'];
+    $price = $room['price'];
+    $capacity = $room['capacity'];
+
+    echo "<li>\n";
+    echo "Room: $room_number | Price: \$$price | Capacity: $capacity";
+    echo "<button>Book</button>";
+    echo "<li>";
+
+}
+
 ?>
+</ul>
 
 </body>
 </html>
