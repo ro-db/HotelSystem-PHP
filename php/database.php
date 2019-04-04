b2ae5f8d59c3b2097a30db733c1cbba87b6ebd15
OmarAlmo <omar.almoallim@gmail.com>
2019-04-04 14:40:49 -0400

    work done on employee rent page, issue verifying dates

--- php/database.php
+++ php/database.php
@@ -1,10 +1,11 @@
 <?php
 
-    $db_conf = parse_ini_file(".db.ini");
+    // $dbs_conf = parse_ini_file(".db.ini");
 
     // Connecting, selecting database
-    $dbconn = pg_connect("host=" . $db_conf["host"] . " dbname=" . $db_conf["dbname"] . " port=" . $db_conf["port"] . " user=" . $db_conf["user"] . " password=" . $db_conf["password"]);
-
+    // $dbconn = pg_connect("host=" . $db_conf["host"] . " dbname=" . $db_conf["dbname"] . " port=" . $db_conf["port"] . " user=" . $db_conf["user"] . " password=" . $db_conf["password"]);
+    
+    $dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=oalmo028 user=oalmo028 password=LGOFG89Ybbq");
     $connection_status = pg_connection_status($dbconn);
 
 ?>
