a1b812be693669f6bbf1bcf3dec419923c6c24d2
OmarAlmo <omar.almoallim@gmail.com>
2019-04-05 12:01:38 -0400

    search queries updated with $ instread of @...

--- someQueries.sql
+++ someQueries.sql
@@ -1,19 +1,19 @@
 
 SELECT DISTINCT * FROM room NATURAL JOIN hotel WHERE 
-                city = @city AND 
-                stars >= @minimumStars AND
-                capacity <= @minimumCapacity AND
-                price <= @maximumPrice AND
-                number_of_rooms <= @minimumRooms;
+                city = $city AND 
+                stars >= $minimumStars AND
+                capacity <= $minimumCapacity AND
+                price <= $maximumPrice AND
+                number_of_rooms <= $minimumRooms;
 
     	
 SELECT DISTINCT * FROM room NATURAL JOIN hotel, hotel_chain WHERE 
-	city = @city AND 
-    stars >= @minimumStars AND
-    capacity <= @minimumCapacity AND
-    price <= @maximumPrice AND
-    number_of_rooms <= @minimumRooms;
-	hotel_chain.address = @hotelChain;
+	city = $city AND 
+    stars >= $minimumStars AND
+    capacity <= $minimumCapacity AND
+    price <= $maximumPrice AND
+    number_of_rooms <= $minimumRooms;
+	hotel_chain.address = $hotelChain;
 
 
 	
