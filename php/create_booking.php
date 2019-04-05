d18ab3032db5b68d913e0b7b65ed12011d5473e9
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2019-04-05 18:44:55 -0400

    REVERTED DATABASE.PHP SO THAT IT WORKS NOW.
    - Hours of debugging. I swear.
    - Also fixing up create_booking.php so that the inserts work.

--- php/create_booking.php
+++ php/create_booking.php
@@ -1,14 +1,35 @@
+
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
+
+// $room_id = pg_escape_string($_POST['room_id']);
+// $startDate = pg_escape_string($_POST['startDate']);
+// $endDate = pg_escape_string($_POST['endDate']);
+// $SIN = pg_escape_string($_POST['SIN']);
+// $fullname = pg_escape_string($_POST['fullname']);
+// $address = pg_escape_string($_POST['address']);
 
 $room_id = $_POST['room_id'];
+$startDate = $_POST['startDate'];
+$endDate = $_POST['endDate'];
 $SIN = $_POST['SIN'];
 $fullname = $_POST['fullname'];
 $address = $_POST['address'];
 
-echo "Inserting: room id=$room_id, SIN=$SIN, fullname=$fullname, address=$address";
+// echo '<script>console.log("Inserting: room id="$room_id", SIN="$SIN", fullname="$fullname", address="$address"")</script>\n';
 
-pg_free_result($result);
+include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
+
+// pg_query("set search_path='public'; INSERT INTO test (first) VALUES ('12');");
+
+// ("sin_number", full_name, address)
+// (sin_number, room_id, start_date, end_date)
+
+// INSERT INTO customer VALUES ($SIN, \'$fullname\', \'$address\'); INSERT INTO booking VALUES ($SIN, $room_id, \'$startDate\', \'$endDate\');';
+
+$insert_customer_and_booking_query = 'SET search_path = "HotelSystem";';
+$insert_customer_and_booking_query .= "INSERT INTO customer (SIN_NUMBER, full_name, address) VALUES ('$SIN', '$fullname', '$address');";
+$insert_customer_and_booking_query .= "INSERT INTO booking (SIN_NUMBER, room_id, start_date, end_date) VALUES ('$SIN', '$room_id', '$startDate', '$endDate');";
+
 pg_close($dbconn);
 
 header("Location:/index.html");
--- php/database.php
+++ php/database.php
@@ -1,10 +1,9 @@
 <?php
 
-    // $db_conf = parse_ini_file($_SERVER["DOCUMENT_ROOT"] . "/.db.ini");
+    $db_conf = parse_ini_file($_SERVER["DOCUMENT_ROOT"] . "/.db.ini");
 
     // Connecting, selecting database
-    // $dbconn = pg_connect("host=" . $db_conf["host"] . " dbname=" . $db_conf["dbname"] . " port=" . $db_conf["port"] . " user=" . $db_conf["user"] . " password=" . $db_conf["password"]);
-    $dbconn = pg_connect("host=web0.site.uottawa.ca port=15432 dbname=oalmo028 user=oalmo028 password=MKUMK56Taap");
+    $dbconn = pg_connect("host=" . $db_conf["host"] . " dbname=" . $db_conf["dbname"] . " port=" . $db_conf["port"] . " user=" . $db_conf["user"] . " password=" . $db_conf["password"]);
     $connection_status = pg_connection_status($dbconn);
 
 ?>
