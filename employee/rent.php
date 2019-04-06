93b1c24ee0be77c5f54cc67f0f23cb95333d3bff
OmarAlmo <omar.almoallim@gmail.com>
2019-04-06 15:34:07 -0400

    updated employee rent and queries.

--- employee/rent.php
+++ employee/rent.php
@@ -5,21 +5,18 @@
     </head>
 
     <body>
-        <?php
-            $roomID = $_POST["roomID"];
-        ?>
 
         <h3>Rent room dates:</h3>
         
         <form method='POST' id="requestBookingForm">
             <label>Customer SIN: </label>
-            <input name="SIN" required="required" type="number" pattern="[0-9]{9,9}"/><br/>
+            <input name="SIN" required="required" type="number" pattern="[0-9]{9,9}" value=<?php echo $_POST['SIN'];?>><br/>
 
             <label>Customer Name:</label>
-            <input name="fullname" required="required" type="text"/><br/>
+            <input name="fullname" required="required" type="text" value=<?php echo $_POST['fullname'];?>><br/>
 
             <label>Customer Address: </label>
-            <input name="address" required="required" type="address"/><br/>
+            <input name="address" required="required" type="address" value=<?php echo $_POST['address'];?>><br/>
            
             <label>Start date:</label>
             <input type="date" name="startDate" value=<?php echo $_POST['startDate'];?> /><br/>
@@ -34,7 +31,8 @@
         <?php
             $today =  date('Y-m-d H:i:s');
 
-            $sin = $_POST['SIN'];
+            $room_id = $_POST["roomID"];
+            $SIN = $_POST['SIN'];
             $fullname = $_POST['fullname'];
             $address = $_POST['address'];
 
@@ -60,7 +58,7 @@
                 // calculate price
                 include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
 
-                $room_price = pg_query('SET search_path="HotelSystem"; SELECT price FROM room WHERE room_id=4;');
+                $room_price = pg_query('SET search_path="HotelSystem"; SELECT price FROM room WHERE room_id=room_id;');
                 while ($price_array = pg_fetch_row($room_price, null, PGSQL_NUM)){
                     $price_per_stay = $price_array[0];
                 }
@@ -73,16 +71,19 @@
         Price:<?php echo" ". $price ?>
 
         <form id="createRentingForm">
-            <button type='submit' name="submit" formaction='/php/rent_complete.php'n>Rent</button>
+            <button type='submit' name="rent" formaction='/php/rent_complete.php'n>Rent</button>
         </form>
 
         <?php
-            if ( isset($_POST['submit']) ){
+            if ( isset($_POST['rent']) ){
                 
                 include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
-            
-                $create_renting=pg_query('SET search_path="HotelSystem"; INSERT INTO rental (sin, room_id, start_date, end_date, price) 
-                                        VALUES ($customer_sin, $room_id, $start_date, $end_date, $price)');
+                
+                $new_customer = array("sin_number" => $SIN, "full_name" => $fullname, "address" => $address);
+                $new_renting = array("sin_number" => $SIN, "room_id" => $room_id, "start_date" => $startDate, "end_date" => $endDate, "price" => $price);
+
+                pg_insert($dbconn, "HotelSystem.customer", $new_customer);
+                pg_insert($dbconn, "HotelSystem.rental", $new_renting);
                 
                 pg_close($dbconn);
             
