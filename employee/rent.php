fee99351b3e3ca5cec0c7e45eaac408d50048ef8
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2019-04-06 19:14:54 -0400

    Added checkout (empty for now).
    - Made employee renting work more elegantly, including redirect.
    - Made rent_complete do the work of the creating the customer and rental (it also shows a page, although it REALLY should not).
    - Update index to give access to new pages.

--- employee/rent.php
+++ employee/rent.php
@@ -8,8 +8,9 @@
 
         <h3>Rent room dates:</h3>
         
-        <form method='POST' id="requestBookingForm">
+        <form method='POST'>
             <label>Customer SIN: </label>
+            <input type='hidden' name='room_id' value=<?php echo $_POST['room_id'];?>/>
             <input name="SIN" required="required" type="number" pattern="[0-9]{9,9}" value=<?php echo $_POST['SIN'];?>><br/>
 
             <label>Customer Name:</label>
@@ -24,74 +25,64 @@
             <label>End date :</label>
             <input type="date" name="endDate" value=<?php echo $_POST['endDate'];?> /><br/>
 
-            <button type="submit" name="submit" value="submit">Submit</button>
+            <button type="submit" name="submit" value="submit">Calculate Price</button>
         
         </form>
 
         <?php
+
+            if(empty($_POST)) {
+                return;
+            }
+
             $today =  date('Y-m-d H:i:s');
 
-            $room_id = $_POST["roomID"];
+            $room_id = $_POST["room_id"];
             $SIN = $_POST['SIN'];
             $fullname = $_POST['fullname'];
             $address = $_POST['address'];
 
-            if ( isset($_POST['submit']) ){
-                $startDate = $_POST['startDate'];
-                $endDate = $_POST['endDate'];
-                
-                if($startDate < $today){
-                    $startDate = date('Y-m-d');                    
-                }
-                if ($endDate < $today){
-                    $endDate = date('Y-m-d');
-                }
-                if ($endDate < $startDate){
-                    $endDate = $startDate;
-                }
-                
-                $datesDifference =  floor(abs(strtotime($endDate) - strtotime($startDate)) / 86400);
-
-                $_POST['startDate'];
-                $_POST['endDate'];
-
-                // calculate price
-                include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
-
-                $room_price = pg_query('SET search_path="HotelSystem"; SELECT price FROM room WHERE room_id=room_id;');
-                while ($price_array = pg_fetch_row($room_price, null, PGSQL_NUM)){
-                    $price_per_stay = $price_array[0];
-                }
-                $price = $datesDifference * $price_per_stay;
-                pg_close($dbconn);
+            $startDate = $_POST['startDate'];
+            $endDate = $_POST['endDate'];
+            
+            if($startDate < $today){
+                $startDate = date('Y-m-d');                    
             }
+            if ($endDate < $today){
+                $endDate = date('Y-m-d');
+            }
+            if ($endDate < $startDate){
+                $endDate = $startDate;
+            }
+            
+            $datesDifference =  floor(abs(strtotime($endDate) - strtotime($startDate)) / 86400);
+
+            // calculate price
+            include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
+
+            $room_price = pg_query('SET search_path="HotelSystem"; SELECT price FROM room WHERE room_id=room_id;');
+            while ($price_array = pg_fetch_row($room_price, null, PGSQL_NUM)){
+                $price_per_stay = $price_array[0];
+            }
+            
+            $price = $datesDifference * $price_per_stay;
+
+            pg_free_result($room_price);
+            pg_close($dbconn);
+
         ?>
 
         <br/>
-        Price:<?php echo" ". $price ?>
 
-        <form id="createRentingForm">
-            <button type='submit' name="rent" formaction='/php/rent_complete.php'n>Rent</button>
-        </form>
-
+        <form method='POST'>
         <?php
-            if ( isset($_POST['rent']) ){
-                
-                include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
-                
-                $new_customer = array("sin_number" => $SIN, "full_name" => $fullname, "address" => $address);
-                $new_renting = array("sin_number" => $SIN, "room_id" => $room_id, "start_date" => $startDate, "end_date" => $endDate, "price" => $price);
-
-                pg_insert($dbconn, "HotelSystem.customer", $new_customer);
-                pg_insert($dbconn, "HotelSystem.rental", $new_renting);
-                
-                pg_close($dbconn);
-            
-                exit();
-
-            }
+        if(isset($price)) {
+            echo "<input type='hidden' name='price' value='$price'/>\n";
+            echo "Price: $" . $price . "<br/>\n";
+            echo "<button type='submit' formaction='/php/rent_complete.php'>Rent</button>\n";
+        }
         ?>
-
+        </form>
 
     </body>
 
--- php/rent_complete.php
+++ php/rent_complete.php
@@ -1,3 +1,18 @@
+
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
+                
+$new_customer = array("sin_number" => $SIN, "full_name" => $fullname, "address" => $address);
+$new_renting = array("sin_number" => $SIN, "room_id" => $room_id, "start_date" => $startDate, "end_date" => $endDate, "price" => $price);
+
+pg_insert($dbconn, "HotelSystem.customer", $new_customer);
+pg_insert($dbconn, "HotelSystem.rental", $new_renting);
+
+pg_close($dbconn);
+
+?>
+
 Rent is complete.
 <br/>
 
