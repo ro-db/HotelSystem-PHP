08484d147c56dbda29e37862223c6111e39e436b
OmarAlmo <omar.almoallim@gmail.com>
2019-04-05 20:59:14 -0400

    created create rent, issues with script running for employee rent page

--- employee/rent.php
+++ employee/rent.php
@@ -3,6 +3,7 @@
     <head>
         <title>Rent</title>
         <script type="text/javascript" src="/javascript/form_handler.js"></script>
+        <script type="text/javascript" src="/javascript/dateVaildator.js"></script>
     </head>
 
     <body>
@@ -12,46 +13,31 @@
 
         <h3>Rent room dates:</h3>
         
-        <form method='POST'>
+        <form method='POST' id="requestBookingForm">
 
         <label>Start date:</label>
         <input type="date" name="startDate" />
         <br/>
         <label>End date :</label>
         <input type="date" name="endDate" />
-        
+        <br/>
         <button type="submit" value="submit">Submit</button>
         
         </form>
         
         <br/>
         Price:
-        <?php
-            // include $_SERVER["DOCUMENT_ROOT"] . '/php/database.php';
-            $price = 13;
-            // $get_room_price_query = pg_query('SET search_path = "HotelSystem"; SELECT price FROM room WHERE room_id=1');
-            // while ($price_array = pg_fetch_row($get_room_price_query, null, PGSQL_NUM)){
-            //      $price = (int)$price_array[0]; 
-            // }
-            
-            '<script type="text/javascript">
+        
+
+        <script type="text/javascript">
                 console.log("reached");
                 var form = document.getElementById("requestBookingForm");
-
                 if(form.addEventListener) {
-
                 form.addEventListener("submit", processBookingForm, false);
                 } else if(form.attachEvent) {
                 form.attachEvent("onsubmit", processBookingForm);
                 }
-
-            </script>';
-            echo $price . "<br/>";
-            echo "\tStart Date: " . $_POST["startDate"] . "<br/>";
-            echo "\tEnd Date: " . $_POST["endDate"]. "<br/>";
-            // pg_free_result($result);
-            // pg_close($dbconn);            
-          ?>
+        </script>
 
     </body>
 
--- php/create_renting.php
+++ php/create_renting.php
@@ -2,6 +2,7 @@
 include $_SERVER['DOCUMENT_ROOT'] . "/php/database.php";
 
 $startDate = $_POST['startDate'];
+$endDate = $_POST['endDate'];
 
 pg_free_result($result);
 pg_close($dbconn);
