238dea167d34e44080ad681848651067d8a613b8
OmarAlmo <omar.almoallim@gmail.com>
2019-04-07 13:02:42 -0400

    added view 1 query

--- someQueries.sql
+++ someQueries.sql
@@ -18,10 +18,19 @@ SELECT DISTINCT * FROM room NATURAL JOIN hotel, hotel_chain WHERE TRUE
 
 // Views
 
+CREATE VIEW view1 AS (
+	(SELECT * FROM room NATURAL JOIN hotel 
+	 WHERE NOT EXISTS
+	(SELECT room_id FROM booking WHERE
+	(room.room_id = $room_id) AND ($startDate BETWEEN $startDate AND $endDate) AND ($endDate BETWEEN $startDate AND $endDate))
+	AND 
+	(SELECT room_id FROM rental WHERE
+	(room.room_id = $room_id) AND ($startDate BETWEEN $startDate AND $endDate) AND ($endDate BETWEEN $startDate AND $endDate))
+	))	
+
 CREATE OR REPLACE VIEW view2 AS 
 	SELECT DISTINCT * FROM room NATURAL JOIN hotel WHERE
-	city = $city AND
-	capacity <= $minimumCapacity;
+	city = $city;
 
 	
 
